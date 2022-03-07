1168f20b1cc26a71ab1a7f72c76f81feff3a2c37
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-03-07 23:27:59 +0200

    change App

--- src/App.js
+++ src/App.js
@@ -3,24 +3,130 @@ import PropTypes from "prop-types";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import React, { Component } from "react";
 import Searchbar from "./Components/Searchbar/Searchbar";
+import LoadMore from "../src/Components/Button/Button";
+import Modal from "../src/Components/Modal/Modal";
+import imgNotCorrectly from "../src/images/Упс.jpeg";
+import { ThreeDots } from "react-loader-spinner";
+import fetchApi from "../src/Components/serviseApi.js/fecthApi";
 import ImageGalleryItem from "./Components/ImageGalleryItem/ImageGalleryItem";
 
 class App extends Component {
   state = {
     imgName: "",
+    status: "idle",
+    showModal: false,
+    imageName: [],
+    idImage: 0,
+    name: "",
+    page: 1,
   };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const prevName = prevState.imgName;
+    const nextName = this.state.imgName;
+    if (prevName !== nextName) {
+      this.setState({
+        page: 1,
+        status: "pending",
+      });
+      setTimeout(() => {
+        fetchApi(nextName, this.state.page)
+          .then((res) => {
+            this.setState({
+              imageName: res,
+              name: nextName,
+              page: 2,
+              status: "resolved",
+              idImage: 0,
+            });
+          })
+          .catch((error) => {
+            this.setState({ status: "rejected" });
+          });
+      }, 1000);
+    }
+  }
 
   handleFormSubmit = (painting) => {
     this.setState({ imgName: painting });
   };
 
+  loadMore = (e) => {
+    e.preventDefault();
+    const queryName = this.state.imgName;
+    this.setState({ status: "pending" });
+    setTimeout(() => {
+      fetchApi(queryName, this.state.page)
+        .then((res) => {
+          return this.setState((prevState) => ({
+            page: prevState.page + 1,
+            imageName: [...prevState.imageName, ...res],
+            status: "resolved",
+          }));
+        })
+        .catch((error) => {
+          this.setState({ status: "rejected" });
+        });
+    }, 1000);
+  };
+
+  showModalFunc = (id) => {
+    this.setState({
+      showModal: !this.state.showModal,
+      idImage: id,
+    });
+  };
+
+  toggleModal = () => {
+    this.setState({ showModal: !this.state.showModal });
+  };
+
   render() {
-    const { imgName } = this.state;
+    const { showModal, status, imageName, idImage, imgName } = this.state;
     return (
       <>
         <Searchbar valueSubmit={this.handleFormSubmit} />
         <main className={s.App}>
-          <ImageGalleryItem searchName={imgName} />
+          {status === "resolved" || status === "pending" ? (
+            <>
+              <ImageGalleryItem
+                searchName={imageName}
+                modal={this.showModalFunc}
+              />
+            </>
+          ) : null}
+          {status === "pending" ? (
+            <div className={s.loaderWrapper}>
+              <ThreeDots color="#00BFFF" height={80} width={80} />
+            </div>
+          ) : null}
+          {status === "resolved" ? (
+            <div className={s.containerBtn} onClick={this.loadMore}>
+              <LoadMore />
+            </div>
+          ) : null}
+          {status === "rejected" ? (
+            <>
+              <div className={s.containerDontCorrectly}>
+                <p>
+                  <b>{`Картинку с именем ${imgName} не найдено или они закончились`}</b>
+                </p>
+
+                <img
+                  src={imgNotCorrectly}
+                  alt={"Pictures is not fined"}
+                  width="400px"
+                  height="400px"
+                />
+              </div>
+            </>
+          ) : (
+            ""
+          )}
+
+          {showModal ? (
+            <Modal image={imageName} onClose={this.toggleModal} id={idImage} />
+          ) : null}
         </main>
       </>
     );
@@ -31,4 +137,8 @@ export default App;
 
 App.propTypes = {
   imageName: PropTypes.string,
+  name: PropTypes.string,
+  page: PropTypes.number,
+  status: PropTypes.string,
+  showModal: PropTypes.bool,
 };
--- src/Components/ImageGalleryItem/ImageGalleryItem.js
+++ src/Components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,143 +1,33 @@
 import { Component } from "react";
-import PropTypes from "prop-types";
 import s from "./ImageGalleryItem.module.css";
-import { ThreeDots } from "react-loader-spinner";
 import ItemsCard from "../ImageGallery/ImageGallery";
-import fetchApi from "../serviseApi.js/fecthApi";
-import LoadMore from "../Button/Button";
-import Modal from "../Modal/Modal";
-import imgNotCorrectly from "../../images/Упс.jpeg";
 export default class FetchItems extends Component {
-  state = {
-    imageName: [],
-    name: "",
-    page: 1,
-    status: "idle",
-    showModal: false,
-  };
-
-  async componentDidUpdate(prevProps, prevState) {
-    const prevName = prevProps.searchName;
-    const nextName = this.props.searchName;
-
-    if (prevName !== nextName) {
-      this.setState({
-        page: 1,
-        status: "pending",
-      });
-      setTimeout(() => {
-        fetchApi(nextName, this.state.page)
-          .then((res) => {
-            this.setState({
-              imageName: res,
-              name: nextName,
-              page: 2,
-              status: "resolved",
-              idImage: 0,
-            });
-          })
-          .catch((error) => {
-            this.setState({ status: "rejected" });
-          });
-      }, 1000);
-    }
-  }
-
-  loadMore = (e) => {
-    e.preventDefault();
-    const queryName = this.props.searchName;
-    this.setState({ status: "pending" });
-    setTimeout(() => {
-      fetchApi(queryName, this.state.page)
-        .then((res) => {
-          return this.setState((prevState) => ({
-            page: prevState.page + 1,
-            imageName: [...prevState.imageName, ...res],
-            status: "resolved",
-          }));
-        })
-        .catch((error) => {
-          this.setState({ status: "rejected" });
-        });
-    }, 1000);
-  };
-
   showModal = (e) => {
     const id = Number(e.target.id);
-    this.setState({
-      showModal: true,
-      idImage: id,
-    });
-  };
-
-  toggleModal = () => {
-    this.setState({ showModal: false });
+    this.props.modal(id);
   };
 
   render() {
-    const { imageName, status, showModal, idImage } = this.state;
+    const { searchName } = this.props;
     return (
       <>
-        {status === "resolved" || status === "pending" ? (
-          <>
-            <ItemsCard>
-              {imageName.map(({ id, webformatURL, tags }) => {
-                return (
-                  <li className={s.item} key={id}>
-                    <img
-                      width="300px"
-                      height="250px"
-                      onClick={this.showModal}
-                      id={id}
-                      src={webformatURL}
-                      alt={tags}
-                    />
-                  </li>
-                );
-              })}
-            </ItemsCard>
-          </>
-        ) : null}
-        {status === "pending" ? (
-          <div className={s.loaderWrapper}>
-            <ThreeDots color="#00BFFF" height={80} width={80} />
-          </div>
-        ) : null}
-        {status === "resolved" ? (
-          <div className={s.containerBtn} onClick={this.loadMore}>
-            <LoadMore />
-          </div>
-        ) : null}
-        {status === "rejected" ? (
-          <>
-            <div className={s.containerDontCorrectly}>
-              <p>
-                <b>{`Картинку с именем ${this.props.searchName} не найдено или они закончились`}</b>
-              </p>
-
-              <img
-                src={imgNotCorrectly}
-                alt={"Pictures is not fined"}
-                width="400px"
-                height="400px"
-              />
-            </div>
-          </>
-        ) : (
-          ""
-        )}
-
-        {showModal ? (
-          <Modal image={imageName} onClose={this.toggleModal} id={idImage} />
-        ) : null}
+        <ItemsCard>
+          {searchName.map(({ id, webformatURL, tags }) => {
+            return (
+              <li className={s.item} key={id}>
+                <img
+                  width="300px"
+                  height="250px"
+                  onClick={this.showModal}
+                  id={id}
+                  src={webformatURL}
+                  alt={tags}
+                />
+              </li>
+            );
+          })}
+        </ItemsCard>
       </>
     );
   }
 }
-
-FetchItems.propTypes = {
-  name: PropTypes.string,
-  page: PropTypes.number,
-  status: PropTypes.string,
-  showModal: PropTypes.bool,
-};
